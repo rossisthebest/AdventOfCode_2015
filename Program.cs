a586eab5b75b76806adc6285e7ebd37c2a0f4b17
Ross Townsend <rossisthebest@gmail.com>
2025-08-30 21:22:26 +0100

    Part 2 puzzle solve

--- Program.cs
+++ Program.cs
@@ -10,3 +10,29 @@ int answer = ups - downs;
 
 Console.WriteLine(answer);
 Console.ReadLine();
+
+
+int currFloor = 0;
+int position = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Equals('('))
+    {
+        currFloor++;
+    }
+    else
+    {
+        currFloor--;
+    }
+
+    if (currFloor < 0)
+    {
+        position = i;
+        break;
+    }
+}
+
+int answerDay2 = position + 1; // position is 1 based rather than 0 based
+
+Console.WriteLine(answerDay2);
+Console.ReadLine();
